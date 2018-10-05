d9f76d06501678300bee28006be0f9ef5eba7535
Tóth Csaba <toth.csaba@servantes.hu>
2018-10-05 07:13:07 +0200

    start

---
+++ example/script/example.js
@@ -0,0 +1,8 @@
+HTMLControlGenerator({
+    inputType: "text",
+    parent: "asdf0",
+    id: "valami",
+    class: "valami",
+    change: function(data) {console.log(data)},
+    onclick: function(){console.log("click")}
+});
